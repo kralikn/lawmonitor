29c86f8b7ac1e0879b1534eec3de25214bc00ee6
Králik Norbert <kraaliknorbert@gmail.com>
2024-10-01 13:30:34 +0200

    console.log

--- lib/uploadPdf.tsx
+++ lib/uploadPdf.tsx
@@ -33,6 +33,7 @@ export async function uploadPdf(pdfLink: PdfLink) {
       // const pdfData = await PdfParse(pdfResponse.data)
       if (bucketName) {
         const file = storage.bucket(bucketName).file(fileName);
+        console.log(fileName);
         await file.save(pdfResponse.data, {
           metadata: {
             contentType: 'application/pdf',
